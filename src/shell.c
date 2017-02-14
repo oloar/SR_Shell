7b7b2354253848050f1fb63ea727734539162160
Gaetan Sorin <gaetan.sorin@gmail.com>
2017-02-14 16:03:01 +0100

    Implements fixed number of pipes

--- src/shell.c
+++ src/shell.c
@@ -17,80 +17,93 @@
  * pipe[1] write
  */
 
+
+#define NBPIPES 10
 /* Pipe stuff */
 void xPipe(int *p) {
-	if(pipe(p) < -1){
-		fprintf(stderr, "Erreur lors de l'ouverture du pipe.\n");
-		exit(2);
-	}
+  if(pipe(p) < -1){
+    fprintf(stderr, "Erreur lors de l'ouverture du pipe.\n");
+    exit(2);
+  }
 }
 
 int seqLength(char ***seq) {
-	int i = 0;
-	while(seq[i] != 0)
-		i++;
-	return i;
+  int i = 0;
+  while(seq[i] != 0)
+    i++;
+  return i;
 }
 
+void closePipes(int p[NBPIPES][2], int nbPipes) {
+  for (int i = 0; i < nbPipes - 1; ++i) {
+    close(p[i][0]);
+    close(p[i][1]);
+  }
+}
 int main() {
-	while (1) {
-		struct cmdline *l;
-		int i,
-			switchInOut = 0,
-			seq_len = 0,
-			status,
-			p[2];
-		pid_t pid;
+  while (1) {
+    struct cmdline *l;
+    int seq_len = 0,
+        status;
 
-		printf("> ");
-		l = readcmd();
-		/* If input stream closed, normal termination */
-		if (!l) {
-			printf("exit\n");
-			exit(0);
-		}
+    int p[NBPIPES][2];
+    pid_t pid;
 
-		if (l->err) {
-			/* Syntax error, read another command */
-			printf("error: %s\n", l->err);
-			continue;
-		}
+    printf("> ");
+    l = readcmd();
+    /* If input stream closed, normal termination */
+    if (!l) {
+      printf("exit\n");
+      exit(0);
+    }
 
-		xPipe(p);
-		seq_len = seqLength(l->seq);
+    if (l->err) {
+      /* Syntax error, read another command */
+      printf("error: %s\n", l->err);
+      continue;
+    }
 
-		/* Execute each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			if (strcmp(cmd[0],"exit")){
+    seq_len = seqLength(l->seq);
+    if (seq_len > 1) {
+      for (int i = 0; i < seq_len - 1; ++i){
+        xPipe(p[i]);
+      }
+    }
 
-				pid = fork();
-				if (pid == 0) {
-					// if (i == seq_len - 1) {
-					if (switchInOut) {
-						dup2(p[0], 0);
-						/* TODO: Gestion des pipes successifs
-						 * Deuxième tube nécéssaire.
-						 */
-					}
-					else {
-						dup2(p[1], 1);
-						// close(p[0]);
-					}
-					execvp(cmd[0], cmd);
-					exit(0); // Exit if cmd[0] is not a  correct shell command
-				}
-				else {
-					switchInOut = (switchInOut + 1) % 2;
-					close(p[1]);
-					waitpid(pid, &status, 0);// TODO: Récupération/Gestion des status
-				}
-				printf("\n");
-			}
-			else {
-				exit(0);
-			}
-		}
-	}
+    /* Execute each command of the pipe */
+    for (int i=0; l->seq[i]!=0; i++) {
+      char **cmd = l->seq[i];
+      if (strcmp(cmd[0],"exit")){
+        pid = fork();
+        if (pid == 0) {
+          if (seq_len > 1) {
+            if (i == 0) {
+              dup2(p[i][1], STDOUT_FILENO);
+            }
+            else if (i == seq_len - 1){
+              dup2(p[i-1][0], STDIN_FILENO);
+            }
+            else {
+              dup2(p[i-1][0], STDIN_FILENO);
+              dup2(p[i][1], STDOUT_FILENO);
+            }
+          }
+          
+          closePipes(p, seq_len);
+          execvp(cmd[0], cmd);
+          exit(0); // Exit if cmd[0] is not a  correct shell command
+        }
+        else {
+          close(p[i][1]); 
+          for (int i = 0; l->seq[i] != 0; ++i) {
+            waitpid(-1, &status, 0); // TODO: Récupération/Gestion des status
+          }
+        }
+      }
+      else {
+        exit(0);
+      }
+    }
+  }
 }
 
